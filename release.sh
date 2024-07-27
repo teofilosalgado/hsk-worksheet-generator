d639aa103a9021b6039f80829f44723afc0716a7
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2024-07-27 16:46:36 -0300

    Package releases.

---
+++ release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+    then
+        echo "Missing minimum HSK level parameter" >&2
+        exit 1
+fi
+
+minimum_hsk_level=$1
+
+if [ -z "$2" ]
+    then
+        maximum_hsk_level=$minimum_hsk_level
+else
+    maximum_hsk_level=$2
+fi
+
+for level in $(eval echo {$1..$2});
+do
+    # Get HSK words from AllSet Learning
+    poetry run python -m scrapy crawl AllSetLearning -L WARNING -a hsk=$level -O ./output/hsk_$level.csv;
+
+    # Compile data to PDF
+    typst compile template/main.typ output/hsk_$level.pdf --font-path font --root . --input hsk="$level" --input csv_file_path="../output/hsk_$level.csv";
+done
+
+# Compress .csv files
+zip -qq ./output/hsk.csv.zip ./output/*.csv;
+
+# Compress .pdf files
+zip -qq ./output/hsk.pdf.zip ./output/*.pdf;
